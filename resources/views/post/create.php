9b591e2574a5df0af7050c6415eea397c5bd0ecc
Karima Jaman Moon <karimajaman9876@gmail.com>
2022-09-08 01:11:36 +0600

    first commit

---
+++ resources/views/post/create.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Post Create</title>
+</head>
+<body>
+    <h1>Create New Post</h1>
+    <form action="">
+        <input type="text" name="title" placeholder="Title Here"><br><br>
+        <textarea name="details" id="" cols="30" rows="10" placeholder="Details"></textarea><br><br>
+        <button>Submit</button>
+    </form>
+    
+</body>
+</html>
